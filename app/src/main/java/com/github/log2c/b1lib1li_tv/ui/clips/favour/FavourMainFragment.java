3b479668cd73a4d767861a52befd9b096252126c
log2c <log2cc@gmail.com>
2023-05-21 11:12:26 +0800

    bugfix

--- app/src/main/java/com/github/log2c/b1lib1li_tv/ui/clips/favour/FavourMainFragment.java
+++ app/src/main/java/com/github/log2c/b1lib1li_tv/ui/clips/favour/FavourMainFragment.java
@@ -1,12 +1,10 @@
 package com.github.log2c.b1lib1li_tv.ui.clips.favour;
 
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.palette.graphics.Palette;
 
 import com.github.log2c.b1lib1li_tv.R;
 import com.github.log2c.b1lib1li_tv.databinding.FragmentFavourMainBinding;
@@ -30,13 +28,6 @@ public class FavourMainFragment extends BaseCoreFragment<FavourViewModel, Fragme
     public void initView(@Nullable Bundle bundle) {
         FragmentFavourMainBinding mBinding = getMBinding();
         mFragmentAdapter = new FavourFragmentAdapter(getChildFragmentManager());
-        Palette.from(BitmapFactory.decodeResource(getResources(),
-                        R.drawable.background_image))
-                .generate(palette -> {
-                    if (palette != null && palette.getDarkMutedSwatch() != null) {
-                        mBinding.getRoot().setBackgroundColor(palette.getDarkMutedSwatch().getRgb());
-                    }
-                });
         mBinding.viewpager.setAdapter(mFragmentAdapter);
         mBinding.viewpager.setKeyEventsEnabled(true);
         mBinding.header.setupWithViewPager(mBinding.viewpager);
